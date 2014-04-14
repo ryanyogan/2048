ea81b9762fd306dbbacd5af891f8a308a31b42c4
Ryan Yogan <ryanyogan@gmail.com>
2014-04-13 22:53:50 -0500

    Initial commit, red state

---
+++ app/scripts/game/game.js
@@ -0,0 +1,11 @@
+angular.module('Game', [])
+.service('GameManager', function() {
+  // Create a new game
+  this.newGame = function() {};
+  // Handle the move action
+  this.move = function() {};
+  // Update the score
+  this.updateScore = function(newScore) {};
+  // Are there any moves left?
+  this.movesAvailable = function() {};
+});
